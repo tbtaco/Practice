b235e1f2547fea466ab31736cb177c3202f5e98a
tbtaco <tbtaco@hotmail.com>
2021-11-10 03:25:44 -0600

    Progress

--- Practice/SDVProb1.cs
+++ Practice/SDVProb1.cs
@@ -110,16 +110,94 @@ namespace Practice
         }
         private char[][][] GetAllBestSolutions(char[][] map)
         {
+            if (!IsValid(map))
+                throw new Exception("Input map must have 1 start point and have a complete border containing only X.  " +
+                    "Each array in the 2D array must also be the same length.");
+
+            char[][][] solutions;
+            int numberToTry = CountChars(map, 'E');
 
 
-
-
-
-
-
+            throw new Exception("TODO");
 
 
             return null;
+        }
+        private char[][][] AddSolution(char[][][] solutions, char[][] s)
+        {
+            if (solutions == null)
+                return new char[][][] { s };
+            if(!AlreadyAdded(solutions, s))
+            {
+                char[][][] newSol = new char[solutions.Length + 1][][];
+                for(int i = 0; i < newSol.Length; i++)
+                {
+                    if (i == newSol.Length - 1)
+                        newSol[i] = s;
+                    else
+                        newSol[i] = solutions[i];
+                }
+                return newSol;
+            }
+            return solutions;
+        }
+        private bool AlreadyAdded(char[][][] solutions, char[][] s)
+        {
+            for (int i = 0; i < solutions.Length; i++)
+                if (SolutionsAreTheSame(solutions[i], s))
+                    return true;
+            return false;
+        }
+        private bool SolutionsAreTheSame(char[][] s1, char[][] s2)
+        {
+            if (s1.Length != s2.Length)
+                return false;
+            for(int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i].Length != s2[i].Length)
+                    return false;
+                for(int j = 0; j < s1[i].Length; j++)
+                    if (s1[i][j] != s2[i][j])
+                        return false;
+            }
+            return true;
+        }
+        private bool IsValid(char[][] map)
+        {
+            int length = 0;
+            for(int i = 0; i < map.Length; i++)
+            {
+                if(i == 0)
+                {
+                    length = map[i].Length;
+                    for (int j = 0; j < length; j++)
+                        if (map[i][j] != 'X')
+                            return false;
+                }
+                if (map[i].Length != length)
+                    return false;
+                if (i == map.Length - 1)
+                {
+                    for (int j = 0; j < length; j++)
+                        if (map[i][j] != 'X')
+                            return false;
+                }
+                else
+                {
+                    if (map[i][0] != 'X' || map[i][length - 1] != 'X')
+                        return false;
+                }
+            }
+            return CountChars(map, 'S') == 1;
+        }
+        private int CountChars(char[][] map, char c)
+        {
+            int count = 0;
+            for (int i = 0; i < map.Length; i++)
+                for (int j = 0; j < map[i].Length; j++)
+                    if (map[i][j] == c)
+                        count++;
+            return count;
         }
     }
 }
