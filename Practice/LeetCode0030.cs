00557f6c1e632def14ba138758f75f64b2a55943
tbtaco <tbtaco@hotmail.com>
2022-01-19 16:26:14 -0600

    Progress on LeetCode 30

--- Practice/LeetCode0030.cs
+++ Practice/LeetCode0030.cs
@@ -51,8 +51,22 @@ namespace Practice
     {
         public LeetCode0030()
         {
-            String s = "barfoofoobarthefoobarman";
-            String[] words = new string[] { "bar", "foo", "the" };
+
+            /*
+            At this point I see that I've made the wrong assumption.  Each word can only be used as many times as it appears in
+            the words array.  If "good" appears twice, it will be used twice.  From previous tests, I thought for some reason
+            that words could be used any number of times, but up to the max length, or something like that.  Doesn't make any
+            since now of course.  Next I think I'll be rewriting my code to support key value pairs {word, count} and do things that way.
+            */
+
+
+
+
+
+            //String s = "barfoofoobarthefoobarman";
+            String s = "wordgoodgoodgoodbestword";
+            //String[] words = new string[] { "bar", "foo", "the" };
+            String[] words = new string[] { "word", "good", "best", "good" };
 
             Console.WriteLine("s = " + s);
             Console.Write("words = ");
@@ -76,34 +90,57 @@ namespace Practice
         }
         public IList<int> FindSubstring(string s, string[] words)
         {
-            IList<int> result = new List<int>();
-
             int totalLength = 0;
             for (int i = 0; i < words.Length; i++)
                 totalLength += words[i].Length;
             if (totalLength > s.Length)
-                return result;
+                return new List<int>();
 
-            //I think I'll go through s substrings and check them that way, instead of going through each word possibility
-            //This way it should be rather simple and I can write methods to just check if that substring works with those words
+            List<String> simplifiedWords = new List<String>(); //Remove duplicates
+            for (int i = 0; i < words.Length; i++)
+                if (!simplifiedWords.Contains(words[i]))
+                    simplifiedWords.Add(words[i]);
+            if (simplifiedWords.Count < words.Length)
+            {
+                String[] newWords = new string[simplifiedWords.Count];
+                for (int i = 0; i < newWords.Length; i++)
+                    newWords[i] = simplifiedWords[i];
+                words = newWords;
+            }
 
+            return FindSubstring(s, words, totalLength);
+        }
+        public IList<int> FindSubstring(string s, string[] words, int totalLength)
+        {
+            IList<int> result = new List<int>();
 
-
-
+            for (int i = 0; i + totalLength <= s.Length; i++)
+                if (SubstringExists(s.Substring(i, totalLength), words))
+                    result.Add(i);
 
             return result;
         }
-        private bool FirstLetterAppears(String s, String[] words)
+        private bool SubstringExists(String s, String[] words) //Recursive Method
         {
-            if (s == null || s.Length == 0)
-                return false;
+            if (s.Length == 0 && words.Length == 0) //Exit condition for the recursion
+                return true;
+
             for(int i = 0; i < words.Length; i++)
-            {
-                if (words[i] != null && words[i].Length > 0)
-                    if (s[0] == words[i][0])
-                        return true;
-            }
-            return false;
+                if(words[i] != null && words[i].Length > 0)
+                    if(s.Substring(0, words[i].Length) == words[i])
+                    {
+                        String[] tempWords = new string[words.Length - 1];
+                        int k = 0;
+                        for(int j = 0; j < words.Length; j++)
+                            if(i != j)
+                            {
+                                tempWords[k] = words[j];
+                                k++;
+                            }
+                        if(SubstringExists(s.Substring(words[i].Length, s.Length - words[i].Length), tempWords)) //Recursive Call
+                            return true;
+                    }
+            return false; //Exit condition for the recursion
         }
     }
 }
