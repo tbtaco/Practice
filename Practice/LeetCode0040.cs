caa43df087b88bb0092b91237ea874b73525f545
tbtaco <tbtaco@hotmail.com>
2022-02-22 16:38:28 -0600

    Setting up LeetCode 40

--- Practice/LeetCode0040.cs
+++ Practice/LeetCode0040.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * Tyler Richey
  * LeetCode 40
- * 12/10/2021
+ * 2/22/2022
  */
 
 using System;
@@ -52,9 +52,45 @@ namespace Practice
 {
     class LeetCode0040
     {
+        private const int targetMin = 1;
+        private const int targetMax = 30;
+        private const int candidateMin = 1;
+        private const int candidateMax = 50;
+        private const int candidateLengthMin = 1;
+        private const int candidateLengthMax = 100;
         public LeetCode0040()
         {
-            throw new Exception("TODO");
+            Random r = new Random();
+            r.Next(); r.Next(); r.Next();
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write("Test " + i + ": Candidates: [");
+                int[] candidates = new int[r.Next(candidateLengthMax - candidateLengthMin + 1) + candidateLengthMin];
+                for (int j = 0; j < candidates.Length; j++)
+                {
+                    if (j > 0)
+                        Console.Write(", ");
+                    candidates[j] = r.Next(candidateMax - candidateMin + 1) + candidateMin;
+                    Console.Write(candidates[j]);
+                }
+                int target = r.Next(targetMax - targetMin + 1) + targetMin;
+                Console.Write("] Target: " + target + " Output: [");
+                IList<IList<int>> result = CombinationSum2(candidates, target);
+                for(int j = 0; j < result.Count; j++)
+                {
+                    if (j > 0)
+                        Console.Write(", ");
+                    Console.Write("[");
+                    for(int k = 0; k < result[j].Count; k++)
+                    {
+                        if (k > 0)
+                            Console.Write(", ");
+                        Console.Write(result[j][k]);
+                    }
+                    Console.Write("]");
+                }
+                Console.WriteLine("]");
+            }
         }
         public IList<IList<int>> CombinationSum2(int[] candidates, int target)
         {
