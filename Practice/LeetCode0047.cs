06ccc9a7c6a364ae7d4e05470733784e7fcd0238
tbtaco <tbtaco@hotmail.com>
2022-05-19 16:41:52 -0500

    Test Push

--- Practice/LeetCode0047.cs
+++ Practice/LeetCode0047.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿/*
+ * Tyler Richey
+ * LeetCode 47
+ * 5/19/2022
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+
+Having issues with Git.  Test Push.
+
+
+
+
+
+*/
+
 namespace Practice
 {
     class LeetCode0047
     {
+
     }
 }
