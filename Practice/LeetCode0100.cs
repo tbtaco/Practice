d778c4455953afbe7b8782281d5e523564372053
tbtaco <tbtaco@hotmail.com>
2023-08-13 00:54:53 -0500

    Solved LeetCode 100

---
+++ Practice/LeetCode0100.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Author: Tyler Richey
+ * LeetCode: 100
+ * Title: Same Tree
+ * Description: Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+ * Difficulty: Easy
+ * Status: Solved
+ * Time Complexity: O(n)
+ * Date: 8/13/2023
+ * Notes: 
+ */
+
+using System;
+
+/*
+100. Same Tree
+Easy
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Example 1:
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+
+Example 2:
+Input: p = [1,2], q = [1,null,2]
+Output: false
+
+Example 3:
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+
+Constraints:
+    The number of nodes in both trees is in the range [0, 100].
+    -10^4 <= Node.val <= 10^4
+*/
+
+namespace Practice
+{
+    class LeetCode0100
+    {
+        // Test Cases
+        public LeetCode0100()
+        {
+            try
+            {
+                Console.WriteLine("Input: [1, [2, 3]] and [1, [2, 3]], Output: " +
+                    IsSameTree(new TreeNode(1, new TreeNode(2), new TreeNode(3)), new TreeNode(1, new TreeNode(2), new TreeNode(3))));
+                Console.WriteLine("Input: [1, [2, 3]] and [1, [2, 4]], Output: " +
+                    IsSameTree(new TreeNode(1, new TreeNode(2), new TreeNode(3)), new TreeNode(1, new TreeNode(2), new TreeNode(4))));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: Something seemed to break with that last test.  See below:\n" + e);
+            }
+        }
+        // Solution
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+
+            if (p == null || q == null)
+                return false;
+
+            if (p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right))
+                return true;
+
+            return false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-<!-- Last updated on 8/9/2023 -->
+<!-- Last updated on 8/13/2023 -->
 This repository is where I'll be saving my solutions to [LeetCode](https://leetcode.com/) problems.  You can also see a summary of my LeetCode [here](https://leetcode.com/tbtaco/).
 
 [Program.cs](https://github.com/tbtaco/Practice/blob/master/Practice/Program.cs) - The starting point for this group of solutions.  It runs different LeetCode solutions along with other functions like generating this [README.md](https://github.com/tbtaco/Practice/blob/master/README.md)
 
-Total Solved: 62 (Easy: 19, Medium: 34, Hard: 9)
+Total Solved: 63 (Easy: 20, Medium: 34, Hard: 9)
 
 [LeetCode0001.cs](https://github.com/tbtaco/Practice/blob/master/Practice/LeetCode0001.cs) - Easy - Solved - Two Sum - Given an array of integers, return the indices for the two numbers that add to equal a target.
 <!-- Other info on LeetCode0001.cs - Author: Tyler Richey - Date: 6/23/2021 - Time Complexity: O(n^2) -->
@@ -185,6 +185,11 @@ Total Solved: 62 (Easy: 19, Medium: 34, Hard: 9)
 
 [LeetCode0080.cs](https://github.com/tbtaco/Practice/blob/master/Practice/LeetCode0080.cs) - Medium - Solved - Remove Duplicates from Sorted Array II - Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
 <!-- Other info on LeetCode0080.cs - Author: Tyler Richey - Date: 7/9/2023 - Time Complexity: O(n) -->
+
+...
+
+[LeetCode0100.cs](https://github.com/tbtaco/Practice/blob/master/Practice/LeetCode0100.cs) - Easy - Solved - Same Tree - Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+<!-- Other info on LeetCode0100.cs - Author: Tyler Richey - Date: 8/13/2023 - Time Complexity: O(n) -->
 
 ...
 
